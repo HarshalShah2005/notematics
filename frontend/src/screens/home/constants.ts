22d4ff4554e7410a5870be9067fa3b74b73600ff
HarshalShah2005 <harshalshahhss@gmail.com>
2024-12-25 16:47:40 +0530

    initial commit

---
+++ frontend/src/screens/home/constants.ts
@@ -0,0 +1,14 @@
+const SWATCHES = [
+    "white",  // white
+    "#ee3333",  // red
+    "#e64980",  // pink
+    "#be4bdb",  // purple
+    "#893200",  // brown
+    "#228be6",  // blue
+    "#3333ee",  // dark blue
+    "#00aa00",  // dark green
+    "#fab005",  // yellow
+    "#fd7e14",  // orange
+];
+  
+export { SWATCHES };
